ebbd54b891f8d34a82f68d91b22f012b732cbd26
adistylaili <adistylaili9446@gmail.com>
2022-04-07 21:44:09 +0700

    todolist pinia

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#">GPP System</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <router-link class="nav-link active" to="/profile">Profile</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/login"> Login </router-link>
+          </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/Todolist">Todolist</router-link>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  <router-view />
+</template>
+
+<style>
+.container {
+  max-width: 960px;
+  margin: 0 auto;
+}
+h1 {
+  padding-top: 40px;
+  font-size: 35px;
+}
+</style>
